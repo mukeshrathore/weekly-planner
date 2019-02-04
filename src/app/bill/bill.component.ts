5fc580f1c373ecce0990c286ecfd9d7a436a6c10
Mukesh Rathore <mukesh.rathore86@gmail.com>
2019-02-03 19:48:33 -0500

    adding form to bill component

--- src/app/bill/bill.component.ts
+++ src/app/bill/bill.component.ts
@@ -10,18 +10,69 @@ export class BillComponent implements OnInit {
   billForm: FormGroup;
   constructor(private fb: FormBuilder) { }
 
+  billMedium = [
+    {
+      value: 'chase_cc',
+      viewValue: 'Chase Credit Card'
+    },
+    {
+      value: 'discover_cc',
+      viewValue: 'Discover Credit Card'
+    },
+    {
+      value: 'bofa_cc',
+      viewValue: 'Bank of America Credit Card'
+    },
+    {
+      value: 'bofa_dc',
+      viewValue: 'Bank of America Debit Card'
+    },
+    {
+      value: 'wells_dc',
+      viewValue: 'Wells Fargo Debit Card'
+    }
+  ];
+
+  billCategories = [
+    {
+      value: 'grocery',
+      viewValue: 'Grocery'
+    },
+    {
+      value: 'gas',
+      viewValue: 'Gas'
+    },
+    {
+      value: 'other',
+      viewValue: 'other'
+    },
+  ];
+
+  stores = [
+    {
+      value: 'patel',
+      viewValue: 'Patel Brother'
+    },
+    {
+      value: 'walmart',
+      viewValue: 'Walmart'
+    },
+  ];
+
   ngOnInit() {
     this.createForm();
   }
+
+
   createForm() {
     this.billForm = this.fb.group({
       billDate: [null, Validators.required],
-      billName: [null, Validators.required],
-      billCategory:[null, Validators.required],
+      // billName: [null, Validators.required],
+      billCategory: [null, Validators.required],
       storeName: [null, Validators.required],
       billAmount: [null, Validators.required],
       PayMedium: [null, Validators.required]
-    })
+    });
   }
   addBill() {
 
--- src/app/import-material-module.module.ts
+++ src/app/import-material-module.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatFormFieldModule, MatInputModule, MatButtonModule } from '@angular/material';
+import {
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatSelectModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [],
@@ -8,12 +15,18 @@ import { MatFormFieldModule, MatInputModule, MatButtonModule } from '@angular/ma
     CommonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule
   ],
   exports: [
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule
   ]
 })
 export class ImportMaterialModuleModule { }
